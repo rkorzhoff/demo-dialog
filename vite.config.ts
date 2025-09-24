a5575bd252567230bbc3b349dc40cde59ac56e5b
Robert Korzhov <r.korzhoff@gmail.com>
2025-09-24 14:01:01 +0300

    ci: add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/demo-dialog',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
